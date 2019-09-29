687cf5df21ec8664f5b8728492d2847b30fab4a2
rushil <rushil@cmu.edu>
2019-09-29 15:46:34 -0400

    Update question.js

--- jsQuiz/question.js
+++ jsQuiz/question.js
@@ -1,10 +1,11 @@
 class Question {
 	
-	constructor(text, choices, answer){}
+	constructor(text, choices, answer){
 		this.text = text;
 		this.choices = choices;
 		this.answer = answer;
-}
+	}
+
 	isCorrectAnswer(choice){
 		return this.answer === choice;
 	}
